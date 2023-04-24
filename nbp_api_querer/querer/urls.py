d7d1dbb5c7674eab6785aa60a3ca1759543eb1de
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-04-24 16:32:54 +0200

    Added API queries

--- nbp_api_querer/querer/urls.py
+++ nbp_api_querer/querer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.MainPage.as_view(), name="main_page"),
+     path('exchanges/<str:currency>/<str:date>', views.AverageRate.as_view(), name="avg_rate"),
+     path('exchanges/average/<str:currency>/<int:N>', views.MinMaxAverage.as_view(), name="avg_min_max_rate"),
+     path('exchanges/difference/<str:currency>/<int:N>', views.Difference.as_view(), name="diff_rate"),
 ]
--- nbp_api_querer/querer/views.py
+++ nbp_api_querer/querer/views.py
@@ -1,9 +1,64 @@
 from django.shortcuts import render
 from rest_framework.views import APIView  
-from django.http import JsonResponse  
+from django.http import JsonResponse, HttpResponse
+import re
+import requests
+from workalendar.europe import Poland
+from datetime import datetime
 
-class MainPage(APIView):  
+class AverageRate(APIView):  
+    def get(self, request, *args, **kwargs):
+        # check if date is in correct format
+        url = f"http://api.nbp.pl/api/exchangerates/rates/a/{kwargs['currency']}/{kwargs['date']}/?format=json"
 
- def get(self, request, format=None):
-    return JsonResponse({"message":
-    'HELLO WORLD FROM DJANGO AND DOCKER'})  
+        if not re.match(r'^\d{4}-\d{2}-\d{2}$', kwargs['date']):
+            return HttpResponse("Date format should be YYYY-MM-DD")
+        response=requests.get(url)
+
+        # check if date is a working day
+        cal = Poland()
+        try:
+            if not cal.is_working_day(datetime.strptime(kwargs['date'], '%Y-%m-%d')):
+                return HttpResponse("Date is not a working day")
+        except:
+            return HttpResponse("Invalid date format")
+
+        try:
+            response = response.json()
+        except:
+            return HttpResponse("Invalid date or currency code")
+        
+        return HttpResponse(response['rates'][0]['mid'])
+       
+ 
+class MinMaxAverage(APIView):
+    def get(self, request, format=JsonResponse, *args, **kwargs):
+        if kwargs['N'] > 255:
+            return HttpResponse("Maximal value of last days (N) is 255")
+
+        url = f"http://api.nbp.pl/api/exchangerates/rates/a/{kwargs['currency']}/last/{kwargs['N']}/?format=json"
+        response = requests.get(url)
+        try:
+            response = response.json()
+        except:
+            return HttpResponse("Invalid currency code")
+        
+        mids = [rate['mid'] for rate in response['rates']]
+        return JsonResponse({"min": min(mids), "max": max(mids)})
+
+
+class Difference(APIView):
+    def get(self, request, format=JsonResponse, *args, **kwargs):
+        if kwargs['N'] > 255:
+            return HttpResponse("Maximal value of last days (N) is 255")
+        url = f"http://api.nbp.pl/api/exchangerates/rates/c/{kwargs['currency']}/last/{kwargs['N']}/?format=json"
+        response = requests.get(url)
+        try:
+            response = response.json()
+        except:
+            return HttpResponse("Invalid currency code")
+        
+        differences = [rate['ask'] - rate['bid'] for rate in response['rates']]
+        return JsonResponse({"max_diff": round(max(differences),4)})
+       
+
